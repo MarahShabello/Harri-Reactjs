e5470e3285ad5149508020f6ab511e6505a4b173
MarahShabello <s11819990@stu.najah.edu>
2023-04-10 10:44:25 +0300

    header

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,25 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import BoxSize from 'react-box-size';
 import ThemeButton from './themeButton';
 import { styled } from '@mui/material/styles';
 import { AppBar, Box, Toolbar, Typography } from '@mui/material';
-import { AppTheme } from '../pages/home';
-
-const StyledAppBar = styled(AppBar)(() => ({
-  backgroundColor: '#fff',
-  width: '100%',
-  position: 'static',
-  border: 'none',
-  boxShadow: '1px 1px 8px lightgray',
-}));
-
-const StyledTypography = styled(Typography)(() => ({
-  color: '#111517',
-  fontWeight: '800'
-}));
+import getFromLocalStorage from '../localStorage/getFromLocalStorage';
 
 function AppHeader() {
-  const { darkTheme } = useContext(AppTheme);
+  const darkTheme = getFromLocalStorage('theme');
+
+  const StyledAppBar = styled(AppBar)(() => ({
+    backgroundColor: darkTheme === 'dark' ? '#2b3945' : 'white',
+    width: '100%',
+    position: 'static',
+    border: 'none',
+    // boxShadow: '1px 1px 8px lightgray',
+  }));
+  
+  const StyledTypography = styled(Typography)(() => ({
+    color: darkTheme === 'dark' ? '#fafafa' : '#111517',
+    fontWeight: '800'
+  }));
 
   return (
     <Box sx={{ flexGrow: 1 }} className={darkTheme === 'dark' ? 'dark-element' : 'light-element'}>

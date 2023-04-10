b5a6947698631a36b9b81aa1ace131d263ed57b7
MarahShabello <s11819990@stu.najah.edu>
2023-04-10 10:44:52 +0300

    add dark theme

--- src/components/themeButton.js
+++ src/components/themeButton.js
@@ -6,23 +6,23 @@ import { Button } from '@mui/material';
 import { AppTheme } from '../pages/home';
 import setInLocalStorage from '../localStorage/setInLocalStorage';
 
-const StyledButton = styled(Button)(() => ({
-    fontFamily: 'Nunito Sans',
-    fontWeight: '800',
-    fontSize: '13px'
-}));
-
-const StyledDarkModeOutlinedIcon = styled(DarkModeOutlinedIcon)(() => ({
-    marginInlineEnd: '5px'
-}));
-
-const StyledLightModeRoundedIcon = styled(LightModeRoundedIcon)(() => ({
-    marginInlineEnd: '5px'
-}));
-
 function ThemeButton() {
     const [theme, setTheme] = useState('Dark');
     const { darkTheme, setDarkTheme } = useContext(AppTheme);
+
+    const StyledButton = styled(Button)(() => ({
+        fontWeight: '800',
+        fontSize: '13px',
+        color: darkTheme === 'dark' ? '#fafafa' : '#111517',
+    }));
+
+    const StyledDarkModeOutlinedIcon = styled(DarkModeOutlinedIcon)(() => ({
+        marginInlineEnd: '5px'
+    }));
+
+    const StyledLightModeRoundedIcon = styled(LightModeRoundedIcon)(() => ({
+        marginInlineEnd: '5px'
+    }));
 
     const switchTheme = () => {
         setTheme(darkTheme)
